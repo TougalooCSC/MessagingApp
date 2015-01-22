8471b6d9955b6f5ccbc509a2e3c524718d68542c
CiaunniMinor <ccapri90@yahoo.com>
2015-01-22 16:58:08 -0600

    added a message entry form and send method

--- singleview/www/js/controllers.js
+++ singleview/www/js/controllers.js
@@ -1,7 +1,23 @@
 angular.module('starter.controllers', [])
-  .controller('ChatController',function($scope, Users)
+  .controller('ChatController',function($scope, Users, Chats, $ionicScrollDelegate)
   {
   	$scope.messages = ["Hi", "Hello"];
   	$scope.users = Users.all();
+  	$scope.imgUrls = {};
+  	$scope.users.forEach(function(element){
+  		$scope.imgUrls[element.id] = element.image;
+  	});
+  	$scope.currUser = Users.get(2);
+  	$scope.currConversation = Chats.getConversation(2);
+  	$scope.messages = $scope.currConversation.messages;
+  	$scope.messageText = "";
+  	$scope.sendMessage = function(msg){
+  	     // $scope.messages.push({from:$scope.currUser.id, contents: msg, fromUser: true, avatar: $scope.currUser.image});
+  	     var msgData = Chats.addMessage($scope.currConversation.id, $scope.currUser.id, $scope.participants, msg);
+  	     msgData["fromUser"] = true;
+  	     msgData["avatar"]   = $scope.currUser.image;
+  	     $scope.msgText = "";
+  	     $ionicScrollDelegate.scrollBottom(true);
+  	   };
 
   });
