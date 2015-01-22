40bfbe79d3392eb491273e3c0e0954d9206b381b
CiaunniMinor <ccapri90@yahoo.com>
2015-01-22 16:57:00 -0600

    added a service to manage conversation

--- singleview/www/js/services.js
+++ singleview/www/js/services.js
@@ -17,5 +17,47 @@ angular.module('starter.services', [])
 				}
 			}
 		};
+		this.getUserByName = function (name) {
+		      for (var i = 0, len = this.users.length; i < len; i++){
+		              if (name === this.users[i])
+		              {
+		                      return this.users[i];
+		              }
+		      }
+		    };
 
-	});
+	})
+	.service('Chats', function () {
+	    this.conversations = [
+	      {id: 1, participants: [1, 2], messages: [{from: 1, to: 2, contents: "hi"}, {from: 2, to: 1, contents: "wassup"}]},
+	      {id: 2, participants: [2, 3], messages: [{from: 2, to: 3, contents: "yo!"}, {from: 3, to: 2, contents: "yo!!!"}]}
+	    ];
+	    this.lastConvoId = 2;
+	    this.getConversation = function (id){
+	       for (var j = 0, len = this.conversations.length; j < len; j++) {
+	         if (id === this.conversations[j].id) {
+	           return this.conversations[j];
+	         }
+	       }
+	    };
+	    this.getConversationByParticipantIds = function (participantIds) {
+	            var allParticipantsFound = true;
+	            for (var i = 0, len = this.conversations.length; i < len; i++) {
+	        for (var j = 0, ids_len = this.conversations.length; j < ids_len; j++) {
+	          if (!(participantIds[j] in this.conversations[i].participants)) {
+	                  allParticipantsFound = false;
+	                  break;
+	          }
+	        }
+	        if (allParticipantsFound) {
+	                return this.conversations[i];
+	        }
+	      }
+	    };
+	    this.addMessage = function(id, senderId, receiverIds, contents){
+	      var convo = this.getConversation(id);
+	      var newMsg = {from: senderId, to: receiverIds, contents: contents};
+	      convo.messages.push(newMsg);
+	      return newMsg;
+	    };
+	  }); 
