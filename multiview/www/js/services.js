d3c875bbe696281028c8476381c3b83f9043d80b
Cornelius Toole <cornelius.toole@gmail.com>
2015-03-27 13:28:45 -0500

    integrating Chats service with the Conversations resource in the firebase

--- multiview/www/js/services.js
+++ multiview/www/js/services.js
@@ -135,36 +135,21 @@ angular.module('starter.services', [])
 
 //   };
 //  })
-.factory('Chats', function() {
+.factory('Chats', function(Base, $firebaseObject, $firebaseArray) {
   // Might use a resource here that returns a JSON array
-
+  var url = Base.getURL();
+  var authData = Base.getAuthData();
   // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlin',
-    lastText: 'Did you get the ice cream?',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }];
+  var ref = new Firebase(url);
+  var conversationsRef = ref.child('conversations');
+  var chatsRef = Base.getUserRef().child('conversations');
+  var chats = $firebaseArray(chatsRef);
+  // var chats = [{
+  //   id: 0,
+  //   name: 'Ben Sparrow',
+  //   lastText: 'You on your way?',
+  //   face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  // }];
 
   return {
     all: function() {
@@ -180,6 +165,10 @@ angular.module('starter.services', [])
         }
       }
       return null;
+    },
+    add: function(newConvo) {
+      conversationsRef.child(newConvo.id).set(newConvo);
+      chats.append(newConvo.id);
     }
   }
 })
